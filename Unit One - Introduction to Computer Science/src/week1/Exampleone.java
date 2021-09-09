9921665d3c19989e4d700984f59535dcd38af672
Kathryn Carty <kcarty@bayviewglen.ca>
2021-09-09 13:17:55 -0400

    Test Commit

--- Unit One - Introduction to Computer Science/src/week1/Exampleone.java
+++ Unit One - Introduction to Computer Science/src/week1/Exampleone.java
@@ -5,8 +5,8 @@ package week1;
  */
 public class Exampleone {
     public static void main(String[] args) {
-        System.out.println("Welcome to ICS3U - AP"); // system.out.println displays text to the screen and hits enter
-        
+        System.out.println("Welcome to ICS3U - AP!"); // system.out.println displays text to the screen and hits enter
+
     }
 
 }
