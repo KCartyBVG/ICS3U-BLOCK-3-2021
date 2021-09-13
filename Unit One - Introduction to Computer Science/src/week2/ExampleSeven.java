1fb878d13477373427ae06ca8f4ba99f2ffe15d7
Kathryn Carty <kcarty@bayviewglen.ca>
2021-09-13 12:59:06 -0400

    Final modifier and fight casting

---
+++ Unit One - Introduction to Computer Science/src/week2/ExampleSeven.java
@@ -0,0 +1,22 @@
+package week2;
+
+
+/**
+ * final modifier - modifies the variable so that it becomes a constant
+ */
+public class ExampleSeven {
+    public static void main(String[] args) {
+       final int x = 7;
+        // x = 8;  // cannot change a final variable
+
+        final int y;
+        y = 8;
+        //y = 21; // cannot change a final variable
+
+        final int NUMBER_OF_STUDENTS = 13;  //naming convention for constants is UPPER_CASE _ for spaces.
+
+
+
+    }
+
+}
