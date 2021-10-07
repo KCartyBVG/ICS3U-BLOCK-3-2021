be8477f1ec550781af06a8274e6c779b9d149d1f
Kathryn Carty <kcarty@bayviewglen.ca>
2021-10-07 14:43:57 -0400

    While loops

---
+++ Unit Three and Four - Selection and Iteration/src/week6/WhileLoops.java
@@ -0,0 +1,49 @@
+package week6;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        exampleOne();
+        exampleTwo();
+        int numVowels = countVowels("This is a sentence with vowels");
+        System.out.println(countVowels);
+    }
+
+    private static int countVowels(String str) {
+        int numVowels = 0;
+        String vowels = "AEIOUaeiou";
+        int index = 0;
+
+        while (index < str.length()) {
+            String nextChar = str.substring(index, index +1);
+            if (vowels.indexOf(nextChar) >= 0) {
+                numVowels ++;
+            }
+            index++;
+        }
+        return numVowels;
+    }
+
+    private static void exampleTwo() {
+        int num = 1;
+        int sum = 0;
+
+
+        while (num<=100) {
+            sum +=num;
+            num ++;
+        }
+
+        System.out.println(sum);
+    }
+
+    private static void exampleOne() {
+        //purpose of a loop is to repeat code
+
+        int num = 1;
+
+        while (num < 10) {
+            System.out.println(num);
+            num ++;
+        }
+    }
+}
