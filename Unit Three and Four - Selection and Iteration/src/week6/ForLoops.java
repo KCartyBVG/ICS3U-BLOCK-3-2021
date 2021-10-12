d15b1c1c0e61f000633a7bdfeade50b4dcc9c982
Kathryn Carty <kcarty@bayviewglen.ca>
2021-10-12 14:27:20 -0400

    for loops

---
+++ Unit Three and Four - Selection and Iteration/src/week6/ForLoops.java
@@ -0,0 +1,25 @@
+package week6;
+
+public class ForLoops{
+    public static void main(String[] args) {
+        whileLoopsExample2();
+        forLoopsExample();
+    }
+
+    private static void forLoopsExample() {
+        //for (i = 0;i > 0;i++)
+    }
+
+    private static void whileLoopsExample2() {
+        int i = 1;
+        int sum = 0;
+
+        while(1<=100) {
+
+
+            sum+=1;
+            i++;
+        }
+        //System.out.println(sum);
+    }
+}
