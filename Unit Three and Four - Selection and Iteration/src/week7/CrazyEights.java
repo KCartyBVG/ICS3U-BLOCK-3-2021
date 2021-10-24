8e182fd68bc5948a3cfcc96b32090cfd83eb24d0
Kathryn Carty <kcarty@bayviewglen.ca>
2021-10-23 21:39:24 -0400

    crazy eights

--- Unit Three and Four - Selection and Iteration/src/week7/CrazyEights.java
+++ Unit Three and Four - Selection and Iteration/src/week7/CrazyEights.java
@@ -71,8 +71,7 @@ public class CrazyEights {
                 eight = false;
         }
         
-        //loop for each turn?
-        // method to call turns?
+        
         boolean keepPlaying = true;
 
         while (keepPlaying) {
@@ -99,66 +98,100 @@ public class CrazyEights {
     private static String processComputer(String cHand, String topCard) {
         String topSuit = topCard.substring(topCard.length() - 2);
         String topNumber = topCard.substring(0, topCard.length()-1);
+        boolean play = true;
         //7H 8C 3D-6D
         String result = "";
         
         //if suit is found in string, make substring for that card, check that its not eight, take out of hand
         String suitCard = cHand.substring(cHand.indexOf(topSuit)-1, cHand.indexOf(topSuit)+1); // MIGHT NOT WORK IF THERE IS NO SUIT IN THE HAND
         String numCard = cHand.substring(cHand.indexOf(topNumber), cHand.indexOf(topNumber)+2);
+        int pickedUp = 0;
 
-        if (cHand.indexOf(topSuit) >= 0 && suitCard.indexOf("8") < 0){
-            cHand = cHand.substring(0, cHand.indexOf(suitCard)).trim() + " " + cHand.substring(cHand.indexOf(suitCard) + suitCard.length()).trim();
-            result = cHand.trim() + "-" + suitCard;
-        }else if (cHand.indexOf(topNumber) >= 0 && numCard.indexOf("8") <0 ){        //IF NOT SUIT -> if number is found in string, make substring for that card, check its not an eight, take out of hand
-            cHand = cHand.substring(0, cHand.indexOf(numCard)).trim() + " " + cHand.substring(cHand.indexOf(numCard) + numCard.length()).trim();
-            result = cHand.trim() + "-" + numCard;
-        } else if (cHand.indexOf(topSuit) >= 0) {         // if rule 1 and 2 is false BUT they have an eight, they can play it and change to a suit they have 
-            cHand = cHand.substring(0, cHand.indexOf(suitCard)).trim() + " " + cHand.substring(cHand.indexOf(suitCard) + suitCard.length()).trim();
-            String suit = suitCard.substring(suitCard.length()-2);
-            if (cHand.indexOf(suit) >= 0) {
+
+        while (play){
+            if (cHand.indexOf(topSuit) >= 0 && suitCard.indexOf("8") < 0){
+                cHand = cHand.substring(0, cHand.indexOf(suitCard)).trim() + " " + cHand.substring(cHand.indexOf(suitCard) + suitCard.length()).trim();
                 result = cHand.trim() + "-" + suitCard;
-            } else if (cHand.length()>3){ // find first card in hand (seperate by " "), seperate the number and the suit
-                int space = cHand.indexOf(" ");
-                String firstCard = cHand.substring(0, space);
-                String firstSuit = firstCard.substring(firstCard.length()-2);
-                String firstNum = suitCard.substring(0, suitCard.length()-2);
-                result = cHand.trim() + "-" + firstNum + suit;
-            }else {     //if they have an eight, and its the last card left
-                result = "-" + suitCard;
-            }
-        } else if (cHand.indexOf(numCard) >=0 && numCard.indexOf("8") >=0){
-            cHand = cHand.substring(0, cHand.indexOf(numCard)).trim() + " " + cHand.substring(cHand.indexOf(numCard) + numCard.length()).trim();
-            String suit = numCard.substring(numCard.length()-2);
-            if (cHand.indexOf(suit) >= 0) {
+                play = false;
+            }else if (cHand.indexOf(topNumber) >= 0 && numCard.indexOf("8") <0 ){        //IF NOT SUIT -> if number is found in string, make substring for that card, check its not an eight, take out of hand
+                cHand = cHand.substring(0, cHand.indexOf(numCard)).trim() + " " + cHand.substring(cHand.indexOf(numCard) + numCard.length()).trim();
                 result = cHand.trim() + "-" + numCard;
-            } else if (cHand.length()>3){ // find first card in hand (seperate by " "), seperate the number and the suit
-                int space = cHand.indexOf(" ");
-                String firstCard = cHand.substring(0, space);
-                String firstSuit = firstCard.substring(firstCard.length()-2);
-                String firstNum = numCard.substring(0, numCard.length()-2);
-                result = cHand.trim() + "-" + firstNum + suit;
-            }else {     //if they have an eight, and its the last card left
-                result = "-" + numCard;
+                play = false;
+            } else if (cHand.indexOf(topSuit) >= 0) {         // if rule 1 and 2 is false BUT they have an eight, they can play it and change to a suit they have 
+                cHand = cHand.substring(0, cHand.indexOf(suitCard)).trim() + " " + cHand.substring(cHand.indexOf(suitCard) + suitCard.length()).trim();
+                String suit = suitCard.substring(suitCard.length()-2);
+                if (cHand.indexOf(suit) >= 0) {
+                    result = cHand.trim() + "-" + suitCard;
+                } else if (cHand.length()>3){ // find first card in hand (seperate by " "), seperate the number and the suit
+                    int space = cHand.indexOf(" ");
+                    String firstCard = cHand.substring(0, space);
+                    String firstSuit = firstCard.substring(firstCard.length()-2);
+                    String firstNum = suitCard.substring(0, suitCard.length()-2);
+                    result = cHand.trim() + "-" + firstNum + suit;
+                }else {     //if they have an eight, and its the last card left
+                    result = "-" + suitCard;
+                }
+                play = false;
+            } else if (cHand.indexOf(numCard) >=0 && numCard.indexOf("8") >=0){
+                cHand = cHand.substring(0, cHand.indexOf(numCard)).trim() + " " + cHand.substring(cHand.indexOf(numCard) + numCard.length()).trim();
+                String suit = numCard.substring(numCard.length()-2);
+                if (cHand.indexOf(suit) >= 0) {
+                    result = cHand.trim() + "-" + numCard;
+                } else if (cHand.length()>3){ // find first card in hand (seperate by " "), seperate the number and the suit
+                    int space = cHand.indexOf(" ");
+                    String firstCard = cHand.substring(0, space);
+                    String firstSuit = firstCard.substring(firstCard.length()-2);
+                    String firstNum = numCard.substring(0, numCard.length()-2);
+                    result = cHand.trim() + "-" + firstNum + suit;
+                }else {     //if they have an eight, and its the last card left
+                    result = "-" + numCard;
+                }
+                play = false;
+            } else {         // IF SUIT AND NUMBER ISNT FOUND -> pick up a card and repeat
+                cHand += getCard() + " ";
+                pickedUp++;
+                if (pickedUp >= 5) { 
+                    result = cHand + "-" + topCard;
+                    play = false;
+                }
+
             }
-        } else {         // IF SUIT AND NUMBER ISNT FOUND -> pick up a card and repeat
-            cHand += getCard() + " ";
-
         }
-
-
-        // IF SUIT AND NUMBER ISNT FOUND -> pick up a card and repeat
-        //add picked up cards to hand, int cards picked up (if cardsPickedUp <= 5, the loop will repeat)
-        // IF PICKED UP FIVE CARDS -> end of turn 
-
-
-        // return remaining hand, "-", playedCard
         return result;
     }
 
     private static String processPlayer(String hand, String topCard, Scanner in) {
         System.out.println("Top Card: " + topCard);
         System.out.println("Your Hand: " + hand);
-        System.out.println("Can you play?");
+        String result = "";
+        boolean canPlay = false;
+        int pickedUp = 0;
+        
+        while (!canPlay){
+            System.out.println("Can you play? ([Y]es or [N]o)");
+            String answer = in.nextLine().toUpperCase();
+            if (answer.equals("Yes") || answer.equals("Y")){
+
+                String playedCard = playCard(in, hand);
+                
+                hand = hand.substring(0, hand.indexOf(playedCard)).trim() + " " + hand.substring(hand.indexOf(playedCard) + playedCard.length()).trim();
+                
+                result = hand.trim() + "-" + playedCard;
+                canPlay = true;
+            }else if (answer.equals("No") || answer.equals("N")) {
+                hand += getCard();// give player a new card and display their new hand
+                System.out.println("Your new hand: " + hand);
+                pickedUp++;
+                if (pickedUp >= 5) {
+                    System.out.println("You can't pick up anymore cards.");
+                    canPlay = true; 
+                    result = hand + "-" + topCard;
+                }
+            } else {
+                System.out.println("Invalid Input: [Y]es or [N]o only!");
+            }
+        }
+        
         
         //IF NO PICK UP CARDS (look at processComputer for instructions)
         // make boolean for (canPlay) - ask if they can play
@@ -166,20 +199,29 @@ public class CrazyEights {
 
         //if yes ADD THE FOLLOWING TO AN IF STATEMENT (below comments)
         //IF NO PICK UP CARDS (look at processComputer for instructions)
-
-
-        // after everytime they picked up a card change boolean
-
-        // make playedCard same as topCard before the if statment - if player picked up 5 cards and didnt put down a new card, playedCard is the same
-
-        // if user enters invalid options : check other games and copy 
-        System.out.println("What would you like to play? ");
-        String playedCard = in.nextLine();
-        // "7H 8C 6D 3D"
-        hand = hand.substring(0, hand.indexOf(playedCard)).trim() + " " + hand.substring(hand.indexOf(playedCard) + playedCard.length()).trim();
         
-        return hand.trim() + "-" + playedCard;
+        return result;
     }
+
+    private static String playCard(Scanner in, String playerHand) {
+        final String VALID_CARDS = "AS2S3S4S5S6S7S8S9S10SJSQSKSAC2C3C4C5C6C7C8C9C10CJCQCKCAD2D3D4D5D6D7D8D9D10DJDQDKDAH2H3H4H5H6H7H8H9H10HJHQHKH";
+        boolean validInput = false;
+        String card = "";
+        while (!validInput) {
+           System.out.print("What would you like to play? (ex. KD): ");
+           card = in.nextLine().toUpperCase();
+           
+           if (VALID_CARDS.indexOf(card) < 0) 
+              System.out.println("Not a valid card: " + card);
+           else if (playerHand.indexOf(card) < 0)
+              System.out.println("You don't have a " + card);
+           else if ((playerHand.indexOf(card) >= 0)) 
+              validInput = true;
+        }
+        return card;
+  
+     }
+    
 
     private static String getCard() {
         String card = getFace() + getSuit();
