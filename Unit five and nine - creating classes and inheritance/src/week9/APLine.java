a991077befa943bb05dcc97cb9ccef01fd252b99
Kathryn Carty <kcarty@bayviewglen.ca>
2021-11-04 13:27:17 -0400

    APLine practice

---
+++ Unit five and nine - creating classes and inheritance/src/week9/APLine.java
@@ -0,0 +1,24 @@
+package week9;
+
+public class APLine {
+    private int a;
+    private int b;
+    private int c;
+
+    public APLine(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getSlope(){
+        return -(double) a/b;
+    }
+
+    public boolean isOnLine(int x, int y) {
+        if (0 == a*x + b*y + c) 
+            return true;
+        else
+            return false;
+    }
+}
