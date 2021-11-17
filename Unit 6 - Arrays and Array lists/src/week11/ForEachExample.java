0000be77c74a416995d74f174dcdd7845d975eee
Kathryn Carty <kcarty@bayviewglen.ca>
2021-11-17 13:05:57 -0500

    For Each loops example

---
+++ Unit 6 - Arrays and Array lists/src/week11/ForEachExample.java
@@ -0,0 +1,28 @@
+package week11;
+
+public class ForEachExample {
+    public static void main(String[] args) {
+        int[] arr = {5, 91, 34, 83, 384, 49, 10, 31};
+
+       /* for (int el:arr){ //stores the next element. el would change everytime and print the next element
+            System.out.println(el);
+        }
+        */
+        String[] words = {"Hello", "Goodbye", "Steve", "Tristan", "Henry"};
+
+
+        int numVowels = 0;
+        String vowels = "aeiouAEIOU";
+        for (String w : words) { // gets the next word
+            // iterate through the characters of w
+            for (int i = 0; i<w.length();i++){
+                String letter = w.substring(i, i+1);
+                if (vowels.indexOf(letter) >=0) {
+                    numVowels++;
+                }
+            }
+        }
+
+        System.out.println(numVowels);
+    }
+}
