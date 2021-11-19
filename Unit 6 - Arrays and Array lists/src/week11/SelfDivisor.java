25b21696d4d94c18fa50573eb062815f5ebe20c3
Kathryn Carty <kcarty@bayviewglen.ca>
2021-11-19 13:20:31 -0500

    SelfDivisor

---
+++ Unit 6 - Arrays and Array lists/src/week11/SelfDivisor.java
@@ -0,0 +1,30 @@
+package week11;
+
+public class SelfDivisor{
+
+public static boolean isSelfDivisor(int number){
+    String temp = "" + number;
+    for (int i = 0; i < temp.length(); i++) {
+        int digit = Integer.parseInt()
+    }
+    
+    
+
+}
+
+public static int[] firstNumDivisors(int start, int num){
+    int[] temp = new int[num];
+
+    int count = 0;
+    int i = 0;
+    while(i< temp.length) {
+        if (isSelfDivisor(start)) {
+            temp[i] = start;
+            count++;
+        }
+        start++;
+    }
+
+    return temp;
+}
+}
